94ec30735b38858238b6941c81e5767b050b735f
gokhanceyhan <gokhanceyhan01@gmail.com>
2017-02-14 20:45:00 +0200

    working on queuing system.

---
+++ src/java/com/solver/dataTypes/KnapsackProblem.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.solver.dataTypes;
+
+/**
+ *
+ * @author gokhanceyhan
+ */
+public class KnapsackProblem {
+    private int numOfKnapsacks;
+    private int numOfItems;
+
+    /**
+     * @return the numOfKnapsacks
+     */
+    public int getNumOfKnapsacks() {
+        return numOfKnapsacks;
+    }
+
+    /**
+     * @param numOfKnapsacks the numOfKnapsacks to set
+     */
+    public void setNumOfKnapsacks(int numOfKnapsacks) {
+        this.numOfKnapsacks = numOfKnapsacks;
+    }
+
+    /**
+     * @return the numOfItems
+     */
+    public int getNumOfItems() {
+        return numOfItems;
+    }
+
+    /**
+     * @param numOfItems the numOfItems to set
+     */
+    public void setNumOfItems(int numOfItems) {
+        this.numOfItems = numOfItems;
+    }
+}
